f60a3da6561b7e224cf85773e5a7c9f877c48757
Ayushi Verma <ayushiverma844@gmail.com>
2026-05-10 12:23:34 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int CountOne(int n){
+        int cnt = 0;
+        while(n > 0){
+            n &= n-1;
+            cnt++ ;
+        }
+        return cnt;
+    }
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0;i<=n;i++){
+            ans.push_back(CountOne(i)); // number of ones in binary representation in i
+        }
+        return ans;
+    }
+};
