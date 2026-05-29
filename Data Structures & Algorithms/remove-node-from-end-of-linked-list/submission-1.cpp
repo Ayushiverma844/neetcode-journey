a993a2a129653da917119cdba5903a7316f37963
Ayushi Verma <ayushiverma844@gmail.com>
2026-05-29 10:13:36 +0530

    Add: remove-node-from-end-of-linked-list - submission-1

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    // Function to find length of linked list
+    int len(ListNode* head) {
+        int cnt = 0;
+ 
+        while (head != NULL) {
+            cnt++;
+            head = head->next;
+        }
+
+        return cnt;
+    }
+
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        // Find length of linked list
+        int length = len(head);
+
+        // Edge Case: remove first node
+        if (length == n) {
+            ListNode* temp = head;
+            head = head->next;
+            delete temp;
+            return head;
+        }
+
+        // Reach node before target node
+        int pos = length - n - 1;
+
+        ListNode* curr = head;
+
+        while (pos > 0) {
+            curr = curr->next;
+            pos--;
+        }
+
+        // Remove node
+        ListNode* temp = curr->next->next;
+        delete(curr->next);
+        curr->next = temp;
+
+        //  return head
+        return head;
+    }
+};
